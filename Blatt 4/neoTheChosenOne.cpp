4e6f1aea89a686ca465e764098a47bafccb32490
Unknown <amiculus.umut@hotmail.de>
2017-05-23 18:29:30 +0200

    added ugly Blatt 4-matrix

---
+++ Blatt 4/neoTheChosenOne.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <string>
+
+double neo[3][3];
+
+void matrixIsAGoodMovie(double neo[3][3]);
+void calculateStuff(double neo[3][3]);
+
+int main() {
+
+	matrixIsAGoodMovie(neo);
+	calculateStuff(neo);
+}
+
+void matrixIsAGoodMovie(double neo[3][3])	{
+
+	std::ifstream myfile;
+	std::string file = "neo.txt";
+	myfile.open(file.c_str());
+
+	if(!myfile)
+	{
+	     std::cout << "File does not exist." << std::endl;
+	}
+
+	std::cout << "The matrix is:"<<std::endl;
+	for(unsigned i = 0; i < 3; i++)	{
+
+		 for(unsigned j = 0; j < 3; j++ )	{
+
+		        myfile >> neo[i][j];
+			std::cout << neo[i][j];
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "-------------------------------"<<std::endl;
+	myfile.close();
+}
+
+void calculateStuff(double neo[3][3])	{
+
+	std::pair<double, double> values;
+
+	values.first = neo[0][0]*((neo[1][1]*neo[2][2]) - (neo[2][1]*neo[1][2])) -neo[0][1]*(neo[1][0]*neo[2][2] - neo[2][0]*neo[1][2]) + neo[0][2]*(neo[1][0]*neo[2][1] - neo[2][0]*neo[1][1]);
+
+	double temp = 0;
+
+	for(unsigned i=0; i<3;i++)	{
+
+		temp=std::abs(neo[0][i])+std::abs(neo[1][i])+std::abs(neo[2][i]);
+		if(values.second<temp)
+			values.second = temp;
+
+	}
+	std::cout<< "Determinant: " << values.first << ", norm: " << values.second << std::endl;
+
+}
